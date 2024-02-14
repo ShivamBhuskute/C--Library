3b5e4fa993a9f5267d38ef6973ed8d6c4a96e782
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-02-14 23:01:19 +0530

    patternmatching

---
+++ dsa/patternmatch.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <climits>
+using namespace std;
+
+int f(int l, int r, string s, string t)
+{
+    l = 0, r = 0;
+    int ans = 0;
+    for (int i = 0; i < s.size();i++)
+    {
+        ans++;
+        if (s[l] == t[r])
+        {
+            l++;
+            r++;
+            if (r == t.size())
+            {
+                break;
+            }
+        }
+        else
+        {
+            l++;
+        }
+    }
+    if(r == t.size()) {
+        return ans - r;
+    }
+    return -1;
+}
+
+int main()
+{
+    int l, r = 0;
+    string s = "aaaaasadjdk";
+    string t = "sad";
+    cout << f(l, r, s, t);
+    return 0;
+}
