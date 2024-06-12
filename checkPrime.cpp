4eda89f9b54d294d1bb4c87547c9dea90d98693d
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-06-12 11:55:25 +0530

    checkPrime

---
+++ checkPrime.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <set>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    if (n <= 3)
+    {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0)
+    {
+        return false;
+    }
+    int i = 5;
+    while (i <= sqrt(n))
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+
+int main()
+{
+    int N;
+    cout << "Enter a number N: ";
+    cin >> N;
+
+    if (isPrime(N))
+    {
+        cout << N << " is a prime number.\n";
+    }
+    else
+    {
+        cout << N << " is not a prime number.\n";
+    }
+
+    return 0;
+}
