ee11ea2cd0c5269651b99f89e8ca21b93889f89e
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-03-14 23:42:26 +0530

    stack overflow example

---
+++ advancedCPP/memory/stack_overflow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int stack[10000000];
+    std::cout << "Hi" << stack[0];
+
+    return 0;
+}
+
+// this function will return without printing "Hi" as the overflow occurs
